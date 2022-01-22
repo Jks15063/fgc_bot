0a7f7d6293cdb46222d3524c40130fa2842bdaba
jks15063 <jks15063@gmail.com>
2022-01-21 23:41:10 -0700

    save

--- config/config.exs
+++ config/config.exs
@@ -8,9 +8,7 @@
 import Config
 
 config :nostrum,
-  # The token of your bot as a string
   token: System.get_env("BOT_TOKEN"),
-  # The number of shards you want to run your bot under, or :auto.
   num_shards: :auto
 
 config :fgc_bot,
